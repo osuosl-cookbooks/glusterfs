3e3aa7202c4afc3919bd46f13ef09d3020280c78
Lance Albertson <lance@osuosl.org>
2014-11-23 13:09:04 -0800

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'glusterfs'
+maintainer       'Oregon State University'
+maintainer_email 'chef@osuosl.org'
+license          'Apache 2.0'
+description      'Installs/Configures glusterfs'
+long_description 'Installs/Configures glusterfs'
+version          '0.1.0'
+
